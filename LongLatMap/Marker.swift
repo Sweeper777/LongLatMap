7ebe7a42cddd199def27b0a2833e2c0faf014b92
Sweeper777 <sumulang@gmail.com>
2016-09-11 08:25:29 +0100

    added some stuff to the managed object subclass

--- LongLatMap/Marker.swift
+++ LongLatMap/Marker.swift
@@ -1,17 +1,23 @@
-//
-//  Marker.swift
-//  LongLatMap
-//
-//  Created by Mulang Su on 9/10/16.
-//  Copyright © 2016 Mulang Su. All rights reserved.
-//
-
 import Foundation
 import CoreData
 
 
 class Marker: NSManagedObject {
 
-// Insert code here to add functionality to your managed object subclass
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        longitude = 0
+        latitude = 0
+        desc = ""
+        title = ""
+    }
 
+    convenience init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?, longitude: Double, latitude: Double, desc: String, title: String, color: Int?) {
+        self.init(entity: entity, insertIntoManagedObjectContext: context)
+        self.longitude = longitude
+        self.latitude = latitude
+        self.desc = desc
+        self.title = title
+        self.color = color
+    }
 }

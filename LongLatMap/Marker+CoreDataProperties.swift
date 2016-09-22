6e71517525d9ea92a6eb7399b6208d48f458cba9
Sweeper777 <sumulang@gmail.com>
2016-09-22 13:31:01 +0100

    updated data model

--- LongLatMap/Marker+CoreDataProperties.swift
+++ LongLatMap/Marker+CoreDataProperties.swift
@@ -5,6 +5,7 @@ extension Marker {
 
     @NSManaged var longitude: NSNumber?
     @NSManaged var latitude: NSNumber?
+    @NSManaged var rotation: NSNumber?
     @NSManaged var color: String?
     @NSManaged var title: String?
     @NSManaged var desc: String?

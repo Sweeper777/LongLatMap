94e8abf079550521c04eb004e25ca359a1db499e
Sweeper777 <sumulang@gmail.com>
2016-09-27 07:21:14 +0100

    improved description to show NSEW

--- LongLatMap/Marker.swift
+++ LongLatMap/Marker.swift
@@ -28,8 +28,23 @@ class Marker: NSManagedObject {
     override var description: String {
         let formatter = NumberFormatter()
         formatter.maximumFractionDigits = 5
-        let longitudeStr = formatter.string(from: self.longitude!)!
-        let latitudeStr = formatter.string(from: self.latitude!)!
+        let longitudeStr: String
+        let latitudeStr: String
+        if self.longitude!.doubleValue < 0 {
+            longitudeStr = "\(formatter.string(from: abs(self.longitude!.doubleValue) as NSNumber)!) W"
+        } else if self.longitude!.doubleValue > 0 {
+            longitudeStr = "\(formatter.string(from: self.longitude!)!) E"
+        } else {
+            longitudeStr = formatter.string(from: self.longitude!)!
+        }
+        
+        if self.latitude!.doubleValue < 0 {
+            latitudeStr = "\(formatter.string(from: abs(self.latitude!.doubleValue) as NSNumber)!) S"
+        } else if self.latitude!.doubleValue > 0 {
+            latitudeStr = "\(formatter.string(from: self.latitude!)!) N"
+        } else {
+            latitudeStr = formatter.string(from: self.latitude!)!
+        }
         
         return "\(NSLocalizedString("Longitude:", comment: "")) \(longitudeStr)\n\(NSLocalizedString("Latitude:", comment: "")) \(latitudeStr)"
     }

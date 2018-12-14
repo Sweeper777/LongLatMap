b75bb9ccb2ab40167e36461e4399a4f35f7ff574
Sweeper777 <sumulang@gmail.com>
2018-12-14 07:09:42 +0000

    set presentationMode in LongitudeSelectorRow

--- LongLatMap/LongitudeSelectorRow.swift
+++ LongLatMap/LongitudeSelectorRow.swift
@@ -5,9 +5,7 @@ final class LongitudeSelectorRow: SelectorRow<PushSelectorCell<CLLocationDegrees
     required init(tag: String?, _ initializer: ((LongitudeSelectorRow) -> ())) {
         super.init(tag: tag)
         initializer(self)
-        presentationMode = PresentationMode.show(controllerProvider: ControllerProvider.storyBoard(storyboardId: "LongitudeSelector", storyboardName: "Main", bundle: Bundle.main), onDismiss: {
-            _ in
-        })
+        presentationMode = PresentationMode.segueName(segueName: "selectLongitude", onDismiss: nil)
         displayValueFor = {
             guard let longitude = $0 else { return "" }
             let (degrees, minutes, seconds) = decimalToDMS(longitude)

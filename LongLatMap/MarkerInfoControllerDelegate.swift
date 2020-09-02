05b429cfb1f452037f1d9288c3c1833a4eef09c4
Mulang Su <sumulang@gmail.com>
2020-09-02 08:55:02 +0800

    deleted all the outdated stuff in my code

--- LongLatMap/MarkerInfoControllerDelegate.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-@objc protocol MarkerInfoControllerDelegate {
-    func controllerDismissed(_ markerInfoController: MarkerInfoController)
-    
-    func markerDeleted(_ markerInfoController: MarkerInfoController)
-}
--- LongLatMap/SettingsControllerDelegate.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-@objc protocol SettingsControllerDelegate {
-    func settingsController(_ settingsController: SettingsController, mapTypeChangedTo value: String)
-    
-    func settingsController(_ settingsController: SettingsController, flatMarkerChangedTo value: Bool)
-    
-    func settingsController(_ settingsController: SettingsController, longLatStyleChangedTo value: Int)
-}

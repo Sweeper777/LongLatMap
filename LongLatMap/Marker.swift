4eaf4418b1c52ac823fc4560088d9e2554ce3d61
Mulang Su <sumulang@gmail.com>
2020-09-03 09:19:06 +0800

    added marker realm model

---
+++ LongLatMap/Marker.swift
@@ -0,0 +1,11 @@
+import RealmSwift
+
+class Marker: Object {
+    @objc dynamic var longitude = 0.0
+    @objc dynamic var latitude = 0.0
+    @objc dynamic var rotation = 0.0
+    @objc dynamic var color = "ffffff"
+    @objc dynamic var title = "Unnamed"
+    @objc dynamic var desc = ""
+    
+}

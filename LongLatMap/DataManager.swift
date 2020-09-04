7bebcf4ccf26b86c6ae655387c6649f6e9734698
Mulang Su <sumulang@gmail.com>
2020-09-04 09:57:00 +0800

    added DataManager

---
+++ LongLatMap/DataManager.swift
@@ -0,0 +1,23 @@
+import RealmSwift
+
+class DataManager {
+    let markers: Results<Marker>
+    private let realm: Realm!
+
+    private init() {
+        do {
+            realm = try Realm()
+            markers = realm.objects(Marker.self)
+        } catch let error {
+            print(error)
+            fatalError()
+        }
+    }
+
+    private static var _shared: DataManager?
+
+    static var shared: DataManager {
+        _shared = _shared ?? DataManager()
+        return _shared!
+    }
+}

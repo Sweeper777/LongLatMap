f708f17d320a99e475e03da5496276a2bfd71634
Mulang Su <sumulang@gmail.com>
2020-09-04 09:54:33 +0800

    Reverted back to a light map for now
    This reverts commit b159a16c9940a4f2f6ea73c41139cad9bd94c8dc.

--- LongLatMap/MapViewController.swift
+++ LongLatMap/MapViewController.swift
@@ -6,18 +6,5 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         mapView = GMSMapView()
         view = mapView
-        if traitCollection.userInterfaceStyle == .dark {
-            mapView.mapStyle = try? GMSMapStyle(contentsOfFileURL: Bundle.main.url(forResource: "darkMapStyle", withExtension: "json")!)
-        } else {
-            mapView.mapStyle = nil
-        }
-    }
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        if traitCollection.userInterfaceStyle == .dark {
-            mapView.mapStyle = try? GMSMapStyle(contentsOfFileURL: Bundle.main.url(forResource: "darkMapStyle", withExtension: "json")!)
-        } else {
-            mapView.mapStyle = nil
-        }
     }
 }

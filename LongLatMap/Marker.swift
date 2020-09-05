7fabeb9471d41ec35ddec53ee7ef20017463c11b
Mulang Su <sumulang@gmail.com>
2020-09-05 10:50:06 +0800

    added Marker.id and Marker.location

--- LongLatMap/Marker.swift
+++ LongLatMap/Marker.swift
@@ -1,4 +1,5 @@
 import RealmSwift
+import CoreLocation
 
 class Marker: Object {
     @objc dynamic var longitude = 0.0
@@ -7,5 +8,17 @@ class Marker: Object {
     @objc dynamic var color = "ffffff"
     @objc dynamic var title = "Unnamed"
     @objc dynamic var desc = ""
+    @objc dynamic var id = 0
     
+    override class func primaryKey() -> String? {
+        "id"
+    }
+    
+    var location: CLLocationCoordinate2D {
+        get { .init(latitude: latitude, longitude: longitude) }
+        set {
+            latitude = newValue.latitude
+            longitude = newValue.longitude
+        }
+    }
 }

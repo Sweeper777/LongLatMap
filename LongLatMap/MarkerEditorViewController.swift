f998df8f64f10ce34cb38794da438149f50c442d
Mulang Su <sumulang@gmail.com>
2020-09-06 09:20:59 +0800

    connected up the cancel and done buttons

--- LongLatMap/MarkerEditorViewController.swift
+++ LongLatMap/MarkerEditorViewController.swift
@@ -3,4 +3,15 @@ import Eureka
 
 class MarkerEditorViewController: FormViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func doneTapped() {
+        
+    }
+    
+    @IBAction func cancelTapped() {
+        dismiss(animated: true, completion: nil)
+    }
 }

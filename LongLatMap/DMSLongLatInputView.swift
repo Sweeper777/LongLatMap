af43b3c238b31f2b6e5baef04d77669ba64c69a8
Mulang Su <sumulang@gmail.com>
2020-09-06 11:04:17 +0800

    added subviews of DMSLongLatInputView

--- LongLatMap/DMSLongLatInputView.swift
+++ LongLatMap/DMSLongLatInputView.swift
@@ -16,5 +16,39 @@ class DMSLongLatInputView : UIView {
     }
     
     private func commonInit() {
+        degreeTextField = UITextField()
+        minuteTextField = UITextField()
+        secondTextField = UITextField()
+        [degreeTextField, minuteTextField, secondTextField].forEach { (tf) in
+            tf?.backgroundColor = .tertiarySystemBackground
+            tf?.placeholder = "00"
+            tf?.borderStyle = .none
+            tf?.autocorrectionType = .no
+            tf?.keyboardType = .numberPad
+            tf?.layer.cornerRadius = 3
+            tf?.font = UIFont.monospacedDigitSystemFont(ofSize: 22, weight: .regular)
+        }
+        let degreeLabel = UILabel()
+        degreeLabel.text = "°"
+        let minuteLabel = UILabel()
+        minuteLabel.text = "\""
+        let secondLabel = UILabel()
+        secondLabel.text = "'"
+        [degreeLabel, minuteLabel, secondLabel].forEach {
+            l in l.font = UIFont.systemFont(ofSize: 22)
+        }
+        let stackView = UIStackView(arrangedSubviews: [degreeTextField, degreeLabel, minuteTextField, minuteLabel, secondTextField, secondLabel])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .clear
+    }
 }
 

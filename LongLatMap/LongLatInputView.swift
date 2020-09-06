efce0bff15d881ad35bcef5c60a8f3f9f4d02064
Mulang Su <sumulang@gmail.com>
2020-09-06 11:04:50 +0800

    added subviews of LongLatInputView

--- LongLatMap/LongLatInputView.swift
+++ LongLatMap/LongLatInputView.swift
@@ -15,4 +15,25 @@ class LongLatInputView : UIView {
         commonInit()
     }
     
+    private func commonInit() {
+        modeSelector = UISegmentedControl(items: ["DMS".localised, "Decimal".localised])
+        modeSelector.selectedSegmentIndex = 0
+        okButton = UIButton(type: .custom)
+        okButton.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
+        dmsInput = DMSLongLatInputView()
+        
+        let stackView = UIStackView(arrangedSubviews: [modeSelector, dmsInput, okButton])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        backgroundColor = .secondarySystemBackground
+    }
 }

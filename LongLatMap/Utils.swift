e7311066070bdfc11805d31109c59ec7bc0f16ff
Sweeper777 <sumulang@gmail.com>
2020-09-12 12:25:42 +0800

    fixed DMS Decimal conversion code

--- LongLatMap/Utils.swift
+++ LongLatMap/Utils.swift
@@ -8,16 +8,16 @@ extension String {
 }
 
 func dmsToDecimal(degrees: Int, minutes: Int, seconds: Int, positive: Bool) -> CLLocationDegrees {
-    Double(degrees) + Double(minutes) / 60 + Double(seconds) / 3600 * (positive ? 1 : -1)
+    (Double(degrees) + Double(minutes) / 60 + Double(seconds) / 3600) * (positive ? 1 : -1)
 }
 
 func decimalToDMS(decimalDegrees: CLLocationDegrees) -> (degrees: Int, minutes: Int, seconds: Int, positive: Bool) {
     let positive = decimalDegrees >= 0
     let absVal = abs(decimalDegrees)
     let degrees = Int(absVal)
-    var remainder = decimalDegrees - Double(degrees)
+    var remainder = absVal - Double(degrees)
     let minutes = Int(remainder / (1.0 / 60.0))
     remainder.formRemainder(dividingBy: 1.0 / 60.0)
-    let seconds = Int(remainder / (1.0 / 3600.0))
+    let seconds = Int(round(remainder / (1.0 / 3600.0)))
     return (degrees, minutes, seconds, positive)
 }

d1b519b715bc2a930bcd0767f2b95fb725171825
Sweeper777 <sumulang@gmail.com>
2020-09-14 10:27:41 +0800

    added a test for decimalToDMS

--- LongLatMapTests/LongLatMapTests.swift
+++ LongLatMapTests/LongLatMapTests.swift
@@ -2,5 +2,10 @@ import XCTest
 
 class LongLatMapTests: XCTestCase {
     func testDDtoDMS() throws {
+        let result = decimalToDMS(decimalDegrees: 25.931944)
+        XCTAssertEqual(result.degrees, 25)
+        XCTAssertEqual(result.minutes, 55)
+        XCTAssertEqual(result.seconds, 55)
+        XCTAssertEqual(result.positive, true)
     }
 }

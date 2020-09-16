a252dfc2f99829dcba0b1f12acdfd0b0f553370a
Mulang Su <sumulang@gmail.com>
2020-09-16 10:33:38 +0800

    fixed yet another bug in decimalToDMS

--- LongLatMap/Utils.swift
+++ LongLatMap/Utils.swift
@@ -14,12 +14,20 @@ func dmsToDecimal(degrees: Int, minutes: Int, seconds: Int, positive: Bool) -> C
 func decimalToDMS(decimalDegrees: CLLocationDegrees) -> (degrees: Int, minutes: Int, seconds: Int, positive: Bool) {
     let positive = decimalDegrees >= 0
     let absVal = abs(decimalDegrees)
-    let degrees = Int(absVal)
+    var degrees = Int(absVal)
     var remainder = absVal - Double(degrees)
-    let minutes = Int(remainder / (1.0 / 60.0))
+    var minutes = Int(remainder / (1.0 / 60.0))
     remainder -= Double(minutes) * (1.0 / 60.0)
-    let seconds = Int(round(remainder / (1.0 / 3600.0)))
-    return (degrees, minutes, seconds, positive)
+    var seconds = Int(round(remainder / (1.0 / 3600.0)))
     
+    if seconds >= 60 {
+        seconds -= 60
+        minutes += 1
     }
+    if minutes >= 60 {
+        minutes -= 60
+        degrees += 1
+    }
+    
+    return (degrees, minutes, seconds, positive)
 }

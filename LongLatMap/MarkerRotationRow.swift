3afa1d63a6e48082d42d7349e9dbd357fb28890c
Mulang Su <sumulang@gmail.com>
2020-09-19 09:33:00 +0800

    set up MarkerRotationCell

--- LongLatMap/MarkerRotationRow.swift
+++ LongLatMap/MarkerRotationRow.swift
@@ -1,4 +1,5 @@
 import Eureka
+import GoogleMaps
 
 public class MarkerRotationCell: Cell<Int>, CellType {
 
@@ -7,6 +8,19 @@ public class MarkerRotationCell: Cell<Int>, CellType {
     @IBOutlet var containerView: UIView!
     @IBOutlet var markerView: UIImageView!
     
+    public override func setup() {
+        super.setup()
+        let gestureRecogniser = UIPanGestureRecognizer(target: self, action: #selector(markerPanned(_:)))
+        self.addGestureRecognizer(gestureRecogniser)
+        markerView.layer.anchorPoint = CGPoint(x: 0.5, y: 1.0)
+        markerView.image = GMSMarker.markerImage(with: .red)
+        titleLabel.text = (row as! MarkerRotationRow).titleText
+        selectionStyle = .none
+    }
+    
+    @objc func markerPanned(_ panGR: UIPanGestureRecognizer) {
+    }
+    
 }
 
 public final class MarkerRotationRow: Row<MarkerRotationCell>, RowType {
@@ -14,4 +28,6 @@ public final class MarkerRotationRow: Row<MarkerRotationCell>, RowType {
         super.init(tag: tag)
         cellProvider = CellProvider<MarkerRotationCell>(nibName: "MarkerRotationCell")
     }
+    
+    var titleText: String?
 }

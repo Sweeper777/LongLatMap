2418c01754f17ac17d87d4c072c9aad94a3ada42
Mulang Su <sumulang@gmail.com>
2020-09-30 09:45:49 +0800

    implemented some table view methods

--- LongLatMap/MarkersListViewController.swift
+++ LongLatMap/MarkersListViewController.swift
@@ -4,4 +4,22 @@ import GoogleMaps
 class MarkersListViewController: UITableViewController {
     var allMarkers = DataManager.shared.markers
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        allMarkers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let marker = allMarkers[indexPath.row]
+        cell.textLabel?.text = marker.title
+        let latitudeText = LongLatFormatter.sharedLatitudeFormatter.string(for: marker.latitude)
+        let longitudeText = LongLatFormatter.sharedLongitudeFormatter.string(for: marker.longitude)
+        cell.detailTextLabel?.text = "\(latitudeText) \(longitudeText)"
+        cell.imageView?.image = GMSMarker.markerImage(with: UIColor(hex: marker.color))
+        return cell
+    }
 }

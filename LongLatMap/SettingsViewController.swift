82943d7ff3012f5f54a9eb3f9d596e00f4a9f50b
Mulang Su <sumulang@gmail.com>
2020-10-13 14:45:07 +0800

    added map type row

--- LongLatMap/SettingsViewController.swift
+++ LongLatMap/SettingsViewController.swift
@@ -6,6 +6,14 @@ class SettingsViewController : FormViewController {
         super.viewDidLoad()
         
         title = "Settings".localised
+        form +++ Section("Map Type".localised)
+        <<< SegmentedRow<MapType>(tagMapType) {
+            row in
+            row.options = [.normal, .satellite, .hybrid, .terrain]
+            row.value = MapType(rawValue: UserDefaults.standard.string(forKey: tagMapType) ?? "Normal")!
+            row.cell.segmentedControl.apportionsSegmentWidthsByContent = true
+        }
+        
     }
     
     @IBAction func doneTapped() {

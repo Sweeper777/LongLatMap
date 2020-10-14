aef350b0afe4fa551ad00752e0abd4eecd81922b
Mulang Su <sumulang@gmail.com>
2020-10-14 21:05:33 +0800

    Create UserSettings.swift

---
+++ LongLatMap/UserSettings.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class UserSettings {
+    static var mapType : MapType {
+        get { UserDefaults.standard.string(forKey: tagMapType).flatMap(MapType.init) ?? .normal }
+        set { UserDefaults.standard.set(newValue.rawValue, forKey: tagMapType) }
+    }
+    
+    static var flatMarkers : Bool {
+        get { UserDefaults.standard.bool(forKey: tagFlatMarkers) }
+        set { UserDefaults.standard.set(newValue, forKey: tagFlatMarkers) }
+    }
+    
+    static var longLatStyle : String {
+        get { UserDefaults.standard.string(forKey: tagLonglatStyle) ?? "DMS" }
+        set { UserDefaults.standard.set(newValue, forKey: tagLonglatStyle) }
+    }
+}
